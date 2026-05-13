ae23c4d171fe92380c79a831f702b5ecf74c9d55
Chastity White Rose <chastitywhiterose@gmail.com>
2026-05-13 11:10:36 -0500

    chastelib update

---
+++ c/chastelib_extensions/unistd/main.c
@@ -0,0 +1,47 @@
+#include <unistd.h>
+#include "chastelib-unistd.h"
+
+int main(int argc, char *argv[])
+{
+ int a=0,b;
+
+ radix=16;
+ int_width=1;
+
+ putstr("Official test suite for the C version of chastelib.\n");
+
+ b=strint("100"); /*convert string to an integer*/
+ if(strint_errors) /*if there are errors, print some messages and exit the program*/
+ {
+  putstr("Input string passed to strint function contains errors.\n");
+  putstr("Please fix the invalid characters/radix and try again.\n");
+  return 0;
+ }
+ 
+ while(a<b)
+ {
+  radix=2;
+  int_width=8;
+  putint(a);
+  putstr(" ");
+  radix=16;
+  int_width=2;
+  putint(a);
+  putstr(" ");
+  radix=10;
+  int_width=3;
+  putint(a);
+
+  if(a>=0x20 && a<=0x7E)
+  {
+   putstr(" ");
+   write(1,&a,1);
+  }
+
+  putstr("\n");
+  a+=1;
+ }
+ 
+ _exit(0);
+}
+
